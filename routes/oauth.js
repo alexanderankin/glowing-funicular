8d898a00a932dab8d18bf5c391ea5542614c6dc5
Dave Ankin <alexanderankin@gmail.com>
2017-12-25 22:34:38 -0500

    rewrote auth and saved required information to cookies

--- routes/oauth.js
+++ routes/oauth.js
@@ -3,6 +3,8 @@ var router = express.Router();
 
 var querystring = require('querystring');
 
+var lib = require('../lib');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('oauth', { title: 'Express | oauth endpoint' });
@@ -21,19 +23,33 @@ router.get('/loginpage', loginpageGet = function (req, res, next) {
 
 var loginpagePost;
 router.post('/loginpage', loginpagePost = function (req, res, next) {
-  var code = "code";
-  // res.render('oauth', { title: 'Express | login post', page: `
-  //   <pre>${JSON.stringify(req.body)}</pre>
-  //   <pre>${JSON.stringify(req.query)}</pre>
-  // ` });
-  res.render('oauth', { title: 'Express | login post', page: `
-    <p>redirecting to ${req.session.redirect_uri}?${querystring.stringify({code})}</p>
-  ` });
+  lib.isValidUser(req.body.username, req.body.password, req.session.scope, function (err, value) {
+    if (err) { throw err; }
+    if (!value) { return res.send('goaway'); }
 
+    lib.generateRandom(function (err, value) {
+      var code = value;
+
+      // res.render('oauth', { title: 'Express | login post', page: `
+      //   <pre>${JSON.stringify(req.body)}</pre>
+      //   <pre>${JSON.stringify(req.query)}</pre>
+      // ` });
+      res.render('oauth', { title: 'Express | login post', page: `
+        <pre>${JSON.stringify(req.session)}</pre>
+        <p>redirecting to ${req.session.redirect_uri}?${querystring.stringify({code})}</p>
+      ` });
+    });
+  });
 });
 
+/**
+ * TODO: if (req.query.response_type === "code") {}
+ * 
+ */
 router.get('/authorize', function (req, res, next) {
   req.session.redirect_uri = req.query.redirect_uri;
+  req.session.client_id = req.query.client_id;
+  req.session.scope = req.query.scope;
 
   res.render('oauth', { title: 'Express | authorize get', page: `
     <pre>${JSON.stringify(req.body)}</pre>
